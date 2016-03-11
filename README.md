a1f7669b52c2d99497272a9397dc6205d2c7469f
Emmanuel Krebs <e.prhbs@gmail.com>
2016-03-11 14:44:48 +0100

    Initial commit
    Initial commit

---
+++ README.md
@@ -0,0 +1,36 @@
+# Debug Sharepoint Javascript Sources
+## Description
+
+This chrome extension gives access to the debug-version of the SharePoint Javascript source files.
+
+You can find it on the [chrome web store][1].
+
+This is the work of [Serge ARADJ](https://github.com/orty) with a little help from [Emmanuel KREBS](https://github.com/e-krebs).
+
+
+## How to enable it
+Once installed from the [chrome web store][1], you'll see a new icon appearing next to chrom's menu : 
+this browser action is by default deactivated ![deactivated browser action](icons/icon19_disabled.png).
+
+When you __left-click__ ont his icon, you'll toggle its state.
+When enabled, it is colored
+![deactivated browser action](icons/icon19.png)
+
+## How it works
+Once enabled, what this extension will do is fairly simple.
+All work is done by the [`background.js`](/background.js) file.
+- requests matching one of the following url patterns are listened :
+    ```
+    '*://*.sharepointonline.com/*/_layouts/15/*/*.js',
+    '*://*.sharepoint.com/_layouts/15/*/*.js'
+    ```
+- for each of these requests
+    - if the url is of the form `*.debug.js` or `*.start.js`, nothing is done and the request will be resolved as usual
+    - otherwise, the `.debug.js` version of the url is requested. If it exists (request.status == 200), the request is redirected to this debug version.
+
+That's it !
+
+#License
+[MIT](/LICENSE)
+
+[1]: https://chrome.google.com/webstore/detail/debug-sharepoint-javascri/okfjnbcodjjeenchhkkfblbdnpfnpidm
---
+++ background.js
@@ -0,0 +1,61 @@
+/** global constants */
+const browser = chrome;
+// key to store the 'enabled' flag value in localStorage
+const enabledKey = 'SpJsDebug_enabled';
+const requestFilters = {
+    'urls' : [
+        '*://*.sharepointonline.com/*/_layouts/15/*/*.js',
+        '*://*.sharepoint.com/_layouts/15/*/*.js'
+    ],
+    'types' : [ 'script' ]
+}
+const keepPattern = /(\.debug|start)\.js$/;
+
+/** startup actions  */
+localStorage.setItem(enabledKey, false); // disabling at startup
+browser.browserAction.onClicked.addListener(setEnabled);
+
+/** for changing the request listener status (that will give access to debug-version of js files) */
+function setEnabled() {
+    // switching the actual 'enabled' flag value
+    const enabled = !(localStorage.getItem(enabledKey) == 'true');
+	if (enabled) {
+        // enabled -> add request listener
+		browser.webRequest.onBeforeRequest.addListener(onBeforeRequestListener, requestFilters, ['blocking']);
+	} else {
+        // disabled -> remove request listener
+		browser.webRequest.onBeforeRequest.removeListener(onBeforeRequestListener);
+	}
+    // setting icon according to enabled status
+    browser.browserAction.setIcon({ 'path' : `/icons/icon19${enabled ? '' : '_disabled'}.png` });
+    // saving the enabled flag value
+    localStorage.setItem(enabledKey, enabled);
+}
+
+/** request listener : will give access to debug-version of js files by performing redirection */
+function onBeforeRequestListener(request) {
+	const requestUrl = request.url;
+	const debugUrl = requestUrl.replace(/\.js$/, '.debug.js');
+    let response = {};
+    if (shouldRedirectUrl(requestUrl, debugUrl, keepPattern)) {
+        response.redirectUrl = debugUrl;
+        console.info(`redirecting ${requestUrl} to ${debugUrl}`);
+    } else {
+        console.warn(`not redirecting ${requestUrl}`);
+    }
+    return response;
+}
+
+/**
+ * decide wether to perform redirection from originalUrl to redirectUrl
+ * @param {url} originalUrl url to be redirected from
+ * @param {url} redirectUrl url to redirect to
+ * @param {pattern} pattern if originalUrl matches this pattern, no redirection
+ */
+function shouldRedirectUrl(originalUrl, redirectUrl, pattern) {
+    if (originalUrl.match(pattern)) return false; // if originalUrl matches pattern, keep originalUrl
+	const request = new XMLHttpRequest();
+	request.open('GET', redirectUrl, false); // 'false' makes the request synchronous
+	request.send(null);
+	return (request.status === 200) // if redirectUrl exists keep it, otherwise use originalUrl
+}
